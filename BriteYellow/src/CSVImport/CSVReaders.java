be8bee0d35070f79d60f6cf26a8d83ca6241570b
Thomas Smith <thomas.smith.11@ucl.ac.uk>
2015-01-01 17:57:37 +0000

    Changed file name to run on my mac

--- BriteYellow/src/CSVImport/CSVReaders.java
+++ BriteYellow/src/CSVImport/CSVReaders.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class CSVReaders{
-    static String fn =new String("/Users/thomas/4th-year-project/Tom4YP/src/24th Sept ORDERED.csv"); 
+    static String fn =new String("/Users/thomas/4th-year-project/BriteYellow/src/24th Sept ORDERED.csv"); 
     static int amd = 20;
     static int length = CSVReaders.findLength();
     String[][] data = new String[amd][length];

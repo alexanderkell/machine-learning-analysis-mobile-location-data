4fc9f54657ce89f1cda21bedffba63b6b2539d46
fezbux <fezan.bux.11@ucl.ac.uk>
2015-02-16 16:56:03 +0000

    newer files added

--- BriteYellow/src/CSVImport/CSVReaders.java
+++ BriteYellow/src/CSVImport/CSVReaders.java
@@ -188,5 +188,4 @@ public class CSVReaders{
       public static void main(String[] args){
     	  
       }
-      }
 }
--- BriteYellow/src/splitting/DataSorting.java
+++ BriteYellow/src/splitting/DataSorting.java
@@ -6,15 +6,37 @@ import Maths.DataFormatOperations;
 
 public class DataSorting {
 	String[][] newdat;
+	static int opt = 1;
+	static String fn = new String();
 	
 	public DataSorting(int opt, String fn) throws ParseException {
+				this.opt = opt;
+				this.fn = fn;
 				//create an instance of the DataFormatOperations class
-				DataFormatOperations data = new DataFormatOperations(opt, fn);
+				DataFormatOperations data = new DataFormatOperations(this.opt, this.fn);
 				
 				//gets the full matrix
 				newdat = data.getFull();
 				
+				/*//selects how many lines to print
+				int length = 100;
+				
+				//prints the array
+				for (int k = 0; k < length; k++){
+				for (int l = 0; l < 16; l++) {
+					System.out.print(newdat[l][k] + " || ");
+				}
+					System.out.print("\n");
+				}*/
 		
 	}
+	
+	public DataSorting() throws ParseException{
+		this(opt, fn);
+		
+	}
+	
+	
+	
 }
 

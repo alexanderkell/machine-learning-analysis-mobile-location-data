da1624f8a5207469a262b3b175599e716b4e3ffc
Alexander Kell <alex_kell@hotmail.co.uk>
2015-02-18 17:13:01 +0000

    Upload Main

--- BriteYellow/src/main.java
+++ BriteYellow/src/main.java
@@ -1,14 +1,15 @@
 
 
 import java.io.FileNotFoundException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 import CSVImport.*;
-
 import splitting.*;
+import Maths.*;
 
 public class main {
-	public static void main(String args[]){
+	public static void main(String args[]) throws ParseException{
 		
 		System.out.println("Track machine learning 101");
 		Scanner sc  = new Scanner(System.in);
@@ -18,15 +19,17 @@ public class main {
 		System.out.println("Enter data file path:");
 		String filePath = sc.nextLine();
 		System.out.println("Select from the following options:\n1. Print Phone 1 Data\n2. Print Phone 2 Data\n3. Print Phone 3 Data\n4. Print Phone 4 Data\n5. Print Phone 5 Data\n6. Print Phone 6 Data\n7. Print Raw Data\n");
-		String phoneOpt = sc.nextLine();
-		getArray dat = new getArray();
-		String[][] phoneData = dat.csvToArray(filePath, phoneOpt);
-		System.out.println(phoneData.length);
+		int phoneOpt = sc.nextInt();
 		
+		DataFormatOperations dfo = new DataFormatOperations(phoneOpt,filePath); //Data Processing
 		
+
 		//Split Data into Individual Tracks
+		DataSorting split = new DataSorting(phoneOpt, filePath);
+		String[][] split1 = split.getSort();
 		
-		
+		//Print data
+		DataFormatOperations.plotTrack2(split1, 0, 1, 0.1f);
 		
 		
 	}

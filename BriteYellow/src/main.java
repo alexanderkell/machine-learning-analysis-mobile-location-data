13605d6887ecb7fb60d394d405931d495b5e43f1
Thomas Smith <thomas.smith.11@ucl.ac.uk>
2015-04-03 13:35:26 +0100

    fixed all the shit

--- BriteYellow/src/main.java
+++ BriteYellow/src/main.java
@@ -6,9 +6,7 @@ import java.util.Scanner;
 import maths.*;
 import mysql.*;
 import csvimport.*;
-import dynamodb.DataBaseOperations;
-import dynamodb.PhoneDataDB;
-import dynamodbquery.DataBaseQueries;
+import dynamodb.*;
 import filters.*;
 import filters.jkalman.JKalmanHelper;
 import graphing.PlotTracks;
@@ -19,42 +17,34 @@ public class main {
 	public static void main(String args[]) throws Exception{
 		
 		System.out.println("Track machine learning 101");
-		Scanner sc  = new Scanner(System.in);
-
-/*		System.out.println("Enter data file path:");
-		String filePath = sc.nextLine();
-		insertMySQL.insertXDisp(filePath);
-*/
-		System.out.println("Enter PhoneID String eg: HT25TW5055273593c875a9898b00");
-		String query = sc.nextLine();
-		DataBaseOperations DBO = new DataBaseOperations();
 		
-		ArrayList<PhoneDataDB> outputPhoneDataDB = DBO.queryTable(query);
-		ArrayList<PhoneData> output = DBO.convertFromPhoneDataDB(outputPhoneDataDB);
-		
-		
-//		ArrayList<PhoneData> output = new insertMySQL().query("FilteredData", query);
 
+		//System.out.println("Enter PhoneID String eg: HT25TW5055273593c875a9898b00");
+		//String query = sc.nextLine();
+		DataBaseQueries QueryRaw = new DataBaseQueries("3D_Cloud_Pan_Data");
+		DataBaseOperations DBO = new DataBaseOperations("Processed_Data");
 		
-//		ArrayList<PhoneData> filtered = insertMySQL.query("FilteredData", query);
-
-		sc.close();
-
+		System.out.println("Querying");
+		ArrayList<PhoneDataDB> outputDB = QueryRaw.queryTable("8d32435715629c24a4f3a16b");
+		System.out.println("Converting");
+		ArrayList<PhoneData> output= DBO.convertFrom(outputDB);
+		
+		System.out.println("Filtering");
 		FilterMain filtering = new FilterMain(200, 5, 5, 3);
 		ArrayList<PhoneData> filtered = filtering.FilterTot(output);
 		
 		// Reanalyse the filtered data using DataGetter and store the result in the "newdata" variable
 		DataGetter newdg = new DataGetter(filtered.toArray(new PhoneData[filtered.size()]));
 		PhoneData[] newdata = newdg.getFullPhoneData();
-	
 		
-		System.out.println(newdata.length);
+		DBO.createTable();
 		
-		DBO.createInsertNewTable(newdata);
-		//insertMySQL input = new insertMySQL();
-		//input.insertMyS(newdata, "FilteredData");
-
+		ArrayList<PhoneDataDB> pddb = DBO.convertToPhoneDataDB(newdata);
+		System.out.println("Saving Data...");
+		DBO.batchWrite(pddb);
+		System.out.println("Done Saving Data.");
 		
-		PlotTracks.plotTrack2(output.toArray(new PhoneData[output.size()]),filtered.toArray(new PhoneData[filtered.size()]), PlotTracks.X, PlotTracks.Y, 0.1f);
+		
+		//PlotTracks.plotTrack2(output.toArray(new PhoneData[output.size()]),filtered.toArray(new PhoneData[filtered.size()]), PlotTracks.X, PlotTracks.Y, 0.1f);
 	}
 }
